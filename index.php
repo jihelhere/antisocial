5930e584f80db34b9a8c4f3a4c9e9d530f90fe74
Guillaume Gregoire <guillaume.gregoire.89@gmail.com>
2012-03-17 16:15:57 +0100

    first push

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>
+
+  </header>
+  <div role="main">
+    <?php include 'php/main.php'; ?>
+  </div>
+  <footer>
+
+  </footer>
+
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+  <script src="js/script.js"></script>
+</body>
+</html>
