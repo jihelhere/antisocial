e97df06c204910aa8d3114a7560da560340153b4
Simon <sim.w80@gmail.com>
2012-03-17 21:22:51 +0100

    design

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+var currentId = 0;
+var inc = 0;
 function showLoader(show){
 	if (!show){
-		$(".spinner").animate({"margin-top": "-=400px"},{duration:500, easing:"easeInElastic",queue:true});
+		$(".spinner").animate({"margin-top": "-=400px"},{duration:500, easing:"easeInElastic",queue:true, complete:function(){
+				$(".spinner").remove();
+		}});
 		return;
 	}
 	// Just in case.
@@ -23,34 +27,60 @@ function showLoader(show){
 	};
 	var spinner = new Spinner(opts).spin();
 	$("body").append(spinner.el);
-	$(spinner.el).css("position","relative");
+	$(spinner.el).css("position","absolute");
 	$(spinner.el).css("width","0px");
 	$(spinner.el).css("margin","auto");
 	$(spinner.el).css("margin-top","-100px");
+	$(spinner.el).css("margin-left","50%");
 
      $(spinner.el).animate({"margin-top": "+=400px"} ,{duration:500, easing:"easeOutElastic",queue:true});
 };
 
 function getNextProp(){
+	inc++;
 	return {
-	text : "lorem ipsum dolor sit amet",
-	id : "plop"
+	text : "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet ",
+	id : ""+inc
 	};
 }
 
+function hide(id){
+    	$("#"+id).animate({"margin-left": "+=2000px"},{duration:1000, easing:"linear",queue:false, complete:function(){
+			$("#"+id).remove();
+		}});
+		return;
+}
 
-function showProposition(){
-var proposition = getNextProp();	
-	$("<div='proposition'></div>")
+function show(p){
+	var proposition = $('<div id="'+p.id+'">'+p.text+'</div>');
+	proposition.css("font-size","70px");
+	proposition.css("width","100%");
+	proposition.css("line-height","70px");
+	proposition.css("padding","50px");
+	proposition.css("margin-left","-100px");
+	proposition.css("margin-left","-2000px");
+	proposition.attr("id",p.id);
+	
+	$("body").append(proposition);
+	$("#"+p.id).animate({"margin-left": "+=2000px"},{duration:1000, easing:"linear",queue:false, complete:function(){
+	}});
 }
 
 $(document).ready(function() {
 	$("body").css("width","100%");
 	$("body").css("height","100%");
+	$("body").css("overflow","hidden");
  	$("body").css("background-color","#74C5E3");	
 
-	showLoader(true);
-	showLoader(false);
+	//showLoader(true);
+	//showLoader(false);
+	
+	$("#itSucks").click(function(){
+		hide(currentId);
+		var p = getNextProp();
+		currentId = p.id;
+		show(p);		
+	});
 });
 
 
