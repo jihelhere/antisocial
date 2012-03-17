958194b85555a555250c1e0be9b1c674425e862c
Damien Cornu <damienc.cornu@gmail.com>
2012-03-17 19:32:41 +0100

    Fonction de récup de tweets générique

--- js/twitter.js
+++ js/twitter.js
@@ -1,12 +1,5 @@
-https://search.twitter.com/search.json?q=Sarkozy%20from:nadine__morano&rpp=1
-
-$(function() {
-	getMoranoTweet('sarkozy');
-});
-
-
-function getMoranoTweet(keyword) {
-	$.getJSON('https://search.twitter.com/search.json?q='+keyword+'%20from:nadine__morano&rpp=1&callback=?', function(t){
+function getTweet(username, keyword) {
+	$.getJSON('https://search.twitter.com/search.json?q='+keyword+'%20from:'+username+'&rpp=1&callback=?', function(t){
 		// t contient un tweet
 		if (t.results.length == 0) {
 			return'';
