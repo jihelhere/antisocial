cbec5fe1b2b4d371d5a6aa2281e7d4db481352ad
Damien Cornu <damienc.cornu@gmail.com>
2012-03-19 23:54:54 +0100

    Réduction du temps

--- js/script2.js
+++ js/script2.js
@@ -8,7 +8,7 @@ $(function() {
   var NB_QUESTIONS = 27;
   var QUESTIONS = [];
   var intervalSetter = 0;
-  var timer = 8000;
+  var timer = 7000;
   var moving = false;
 
   var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Fvoxe.org%2Fapi%2Fv1%2Fpropositions%2Fsearch%3FcandidacyIds%3D4f1ec52e6e27d70001000007%2C4f1eddf96e27d7000100008b%2C4f2c143202b7400005000029%2C4f1888db5c664f0001000119%2C4f188a59f8104a0001000004%2C4f1887545c664f000100010f%2C4f1888945c664f0001000116%2C4f188a20f8104a0001000002%2C4f242b3269b233000100002b%22%20and%20itemPath%20%3D%20%22json.response.propositions%22&format=json';
